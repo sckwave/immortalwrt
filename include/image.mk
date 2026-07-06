3f0f9420aa8fbd4c35a87719af5e4a92e9aed065
sckwave <67253854+sckwave@users.noreply.github.com>
2026-07-06 14:31:58 +0800

    Update image.mk

--- include/image.mk
+++ include/image.mk
@@ -428,6 +428,7 @@ else
 	-$(CP) -T $(mkfs_cur_target_dir).opkg/ $(mkfs_cur_target_dir)/etc/opkg/
 	rm -rf $(mkfs_cur_target_dir).opkg $(mkfs_cur_target_dir).conf
 endif
+	chmod 777 $(TOPDIR)/files/etc/modules-boot.d/zzroot
 	$(call prepare_rootfs,$(mkfs_cur_target_dir),$(TOPDIR)/files)
 
 $(KDIR)/root.%: kernel_prepare
